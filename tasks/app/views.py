51cf79fb7b65b6e350e819ac7590f9437f6deff7
targithu <githubtar@gmail.com>
2023-07-02 00:36:52 +0530

    feat:added charts

--- tasks/app/views.py
+++ tasks/app/views.py
@@ -6,36 +6,76 @@ from django.contrib.auth.decorators import login_required
 from django.conf import settings
 from .models import Feedback
 from django.http import HttpResponse
+from django.db.models import Count
 from .forms import FeedbackForm
 # Create your views here.
 @login_required(login_url='/login')
 def feedback(request):
-    if request.method=='POST':
-     form=FeedbackForm(request.POST)
-     if form.is_valid():
-        subject=request.POST['subject']
-        rating=request.POST['rating']
-        email=request.POST['email']
-        comments=request.POST['comments']
-        k=form.save(commit=False)
-        k.user = request.user
-        k.save()
-        try:
-            m1=(f"Feedback Form By {k.user}",f"Subject:{subject} \n Rating:{rating} \n Feedback:{comments} ", email, [settings.EMAIL_HOST_USER,email])
-            m2=("Thank for your response","please keep giving regular feedback",settings.EMAIL_HOST_USER,[email])
-            send_mass_mail((m1, m2), fail_silently=False)
-        except BadHeaderError:
-            return HttpResponse("Invalid header found.")
+    if request.method == 'POST':
+        form = FeedbackForm(request.POST)
+        if form.is_valid():
+            subject = form.cleaned_data['subject']
+            rating = form.cleaned_data['rating']
+            email = form.cleaned_data['email']
+            comments = form.cleaned_data['comments']
 
-        return redirect('/feed')
-    form=FeedbackForm()
-    return render(request,'app/feedback.html',{"form":form})
+            k = form.save(commit=False)
+            k.user = request.user
+            k.save()
+
+            try:
+                m1 = (
+                    f"Feedback Form By {k.user}",
+                    f"Subject: {subject}\nRating: {rating}\nFeedback: {comments}",
+                    settings.EMAIL_HOST_USER,
+                    [email]
+                )
+                m2 = (
+                    "Thank you for your response",
+                    "Please keep giving regular feedback",
+                    settings.EMAIL_HOST_USER,
+                    [email]
+                )
+                send_mass_mail((m1, m2), fail_silently=False)
+            except BadHeaderError:
+                return HttpResponse("Invalid header found.")
+            
+            return redirect('/feed')
+    else:
+        form = FeedbackForm()
+    
+    return render(request, 'app/feedback.html', {"form": form})
 class FeedListView(LoginRequiredMixin,ListView):
-    model=Feedback
-    context_object_name='feed'
+    model = Feedback
+    context_object_name = 'feed'
+    template_name = 'feedback_list.html' 
+
     def get_queryset(self):
         return Feedback.objects.filter(user=self.request.user).order_by('-id')
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        ratings = self.get_queryset().values_list('rating', flat=True)
+
+        feedbacks_count = (
+            Feedback.objects
+            .values('user')
+            .annotate(count=Count('id'))
+            .order_by('-count')
+        )
+
+        labels = ['1⭐', '2⭐', '3⭐', '4⭐', '5⭐']
+        ratings_data = [ratings.filter(rating=i).count() for i in range(1, 6)]
+
+        context['ratings'] = ratings
+        context['labels'] = labels
+        context['ratings_data'] = ratings_data
+        context['feedbacks_count'] = feedbacks_count
+
+        return context
     
 @login_required(login_url='/login')
 def display(request):
     return render(request,'app/display.html')
+
--- tasks/manage.py
+++ tasks/manage.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python
-"""Django's command-line utility for administrative tasks."""
+"""Django's command-line utility for administrative formify."""
 import os
 import sys
 
 
 def main():
-    """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tasks.settings')
+    """Run administrative formify."""
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'formify.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
